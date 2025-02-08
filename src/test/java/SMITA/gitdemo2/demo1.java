cce3bcafd19e8678449fe0cd92d33f57f602d596
smita2696 <smitanimbulkar02@gmail.com>
2025-02-08 16:00:20 +0530

    firstcommit

---
+++ src/test/java/SMITA/gitdemo2/demo1.java
@@ -0,0 +1,11 @@
+package SMITA.gitdemo2;
+
+public class demo1 {
+	
+	public void printing() {
+	System.out.println("this is the landing page3");
+	System.out.println("this is the landing page4");
+	System.out.println("this is the landing page5");
+	}
+	
+}
